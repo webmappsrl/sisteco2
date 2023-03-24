e559156c069ccd01531fa1aa60c2401b5a0efabd
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-24 10:26:09 +0100

    Sisteco:import command created

---
+++ app/Console/Commands/ImportUsers.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ImportUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sisteco2:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import users from Sisteco';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
