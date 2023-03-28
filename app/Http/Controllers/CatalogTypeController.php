88b36d6739aa52d649bd3fd4fcd8fd91fb35facc
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-28 10:23:32 +0200

    CatalogType Model

---
+++ app/Http/Controllers/CatalogTypeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCatalogTypeRequest;
+use App\Http\Requests\UpdateCatalogTypeRequest;
+use App\Models\CatalogType;
+
+class CatalogTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCatalogTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CatalogType $catalogType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CatalogType $catalogType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCatalogTypeRequest $request, CatalogType $catalogType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CatalogType $catalogType)
+    {
+        //
+    }
+}
