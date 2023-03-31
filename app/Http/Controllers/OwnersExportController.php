83850f564a9e62457b9a2a43ca22f1acc1f93f9b
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-31 13:55:10 +0200

    Created OwnersExportController and URL to handle the download of the owners list

---
+++ app/Http/Controllers/OwnersExportController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Exports\OwnersExport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class OwnersExportController extends Controller
+{
+    public function export()
+    {
+        return Excel::download(new OwnersExport(), 'owners.xlsx');
+    }
+}
