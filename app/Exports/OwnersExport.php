633043823ebdc604aa0e3c3dbe3adb94dbd181a3
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-31 14:21:54 +0200

    Added Num TOT and Val TOT columns to the export file

--- app/Exports/OwnersExport.php
+++ app/Exports/OwnersExport.php
@@ -8,8 +8,9 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class OwnersExport implements FromCollection, ShouldAutoSize, WithHeadings, WithEvents
+class OwnersExport implements FromCollection, ShouldAutoSize, WithHeadings, WithEvents, WithMapping
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -17,6 +18,35 @@ class OwnersExport implements FromCollection, ShouldAutoSize, WithHeadings, With
     public function collection()
     {
         return Owner::all();
+    }
+
+    /**
+     * @param Owner $owner
+     * @return array
+     */
+    public function map($owner): array
+    {
+        return [
+            $owner->id,
+            $owner->created_at,
+            $owner->updated_at,
+            $owner->sisteco_id,
+            $owner->first_name,
+            $owner->last_name,
+            $owner->email,
+            $owner->business_name,
+            $owner->vat_number,
+            $owner->fiscal_code,
+            $owner->phone,
+            $owner->{'addr:street'},
+            $owner->{'addr:housenumber'},
+            $owner->{'addr:city'},
+            $owner->{'addr:postcode'},
+            $owner->{'addr:province'},
+            $owner->{'addr:locality'},
+            $owner->cadastralParcels->count(),
+            $owner->cadastralParcels->sum('estimated_value'),
+        ];
     }
 
     public function headings(): array
@@ -39,16 +69,17 @@ class OwnersExport implements FromCollection, ShouldAutoSize, WithHeadings, With
             'Cittá',
             'CAP',
             'Provincia',
-            'Localitá'
-
-
+            'Localitá',
+            'Num TOT',
+            'Val TOT',
         ];
     }
+
     public function registerEvents(): array
     {
         return [
             AfterSheet::class => function (AfterSheet $event) {
-                $event->sheet->getStyle('A1:Q1')->getFont()->setSize(15)->setBold(true);
+                $event->sheet->getStyle('A1:S1')->getFont()->setSize(15)->setBold(true);
             },
         ];
     }
