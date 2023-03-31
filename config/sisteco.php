d873426b1d67117c78aac20983b795ec33d4bd5a
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-31 16:57:55 +0200

    Updated config file for Sisteco with necessary variables

---
+++ config/sisteco.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    'vat' => [
+        'label' => 'IVA',
+        'unit' => 'percentage',
+        'value' => 22,
+    ],
+    'supervision' => [
+        'label' => 'Direzione Lavori',
+        'unit' => 'percentage',
+        'value' => 10,
+    ],
+    'overheads' => [
+        'label' => 'Spese Generali',
+        'unit' => 'percentage',
+        'value' => 16,
+    ],
+    'business_profit' => [
+        'label' => 'Utile di Impresa',
+        'unit' => 'percentage',
+        'value' => 10,
+    ],
+    'intervention_certification' => [
+        'label' => 'Costo certificazione intervento',
+        'unit' => 'fixed',
+        'value' => 1100,
+    ],
+    'team_management' => [
+        'label' => 'Perc. Gestione CDB',
+        'unit' => 'percentage',
+        'value' => 2,
+    ],
+    'platform_maintenance' => [
+        'label' => 'Perc. Mantenimento piattaforma',
+        'unit' => 'percentage',
+        'value' => 2,
+    ],
+    'maintenance' => [
+        'label' => 'Costo manutenzione annua per ettaro',
+        'unit' => 'fixed',
+        'value' => 1000,
+    ],
+    'maintenance_certification' => [
+        'label' => 'Costo certificazione manutenzione',
+        'unit' => 'fixed',
+        'value' => 1000,
+    ],
+];
