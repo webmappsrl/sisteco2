9d969a2574f3527dbb51079b060fad001f9eee6b
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-04-04 17:08:39 +0200

    Co-authored-by: Alessio Piccioli <piccioli@netseven.it>

--- app/Console/Commands/EstimateByCatalog.php
+++ app/Console/Commands/EstimateByCatalog.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Exception;
 use App\Models\Catalog;
 use App\Models\CadastralParcel;
+use Faker\Core\Number;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -70,32 +71,67 @@ class EstimateByCatalog extends Command
             $parcel_code = $p->computeSlopeClass() . '.' . $p->computeTransportClass();
             $total_price = 0;
             //define json structure
-
             $interventions = [];
             $maintenance = [];
             $general = [];
             $json = [];
+
+            $intervention_area = 0;
+            $intervention_price = 0;
             if (count($results) > 0) {
                 $count = count($results);
                 $this->info("Found $count intersections");
                 foreach ($results as $item) {
                     $cod_int = $types[$item->catalog_type_id];
                     $unit_price = $prices[$cod_int][$parcel_code];
-                    $price = $item->area / 10000 * $unit_price * (1 + $vat / 100); //adding the VAT to the price
+                    $price = ($item->area / 1000) * $unit_price * (1 + $vat / 100); //adding the VAT to the price
                     $total_price += $price;
-                    $items[] = [
-                        'code' => $cod_int . '.' . $parcel_code,
-                        'area' => number_format($item->area / 10000, 4, ',', '.'),
-                        'unit_price' => number_format($unit_price, 2, ',', '.'),
-                        'price' => number_format($price, 2, ',', '.'),
-                    ];
-                }
-                //code that starts with '0' should not be present in the catalog
-                //other codes should be present in the catalog even if the area is 0
-                $interventions = array_filter($items, function ($item) {
-                    return substr($item['code'], 0, 1) != '0';
-                });
+                    $intervention_area += $item->area / 1000;
+                    $intervention_price += $price;
 
+                    //if $cod_int is not equal to 0 then add the item to the interventions array
+                    if ($cod_int != 0) {
+                        $items[] = [
+                            'code' => $cod_int . '.' . $parcel_code,
+                            'area' => number_format($item->area / 10000, 4, ',', '.'),
+                            'unit_price' => number_format($unit_price, 2, ',', '.'),
+                            'price' => number_format($price, 2, ',', '.'),
+                        ];
+                        $interventions['items'] = $items;
+                    }
+                }
+
+                //define the variables for the general array
+                $supervision_price = $intervention_price * (1 + config('sisteco.supervision.value') / 100);
+                $overhead_price = $intervention_price * (1 + config('sisteco.overhead.value') / 100);
+                $business_profit_price = $intervention_price * (1 + config('sisteco.business_profit.value') / 100);
+                $intervention_certification = config('sisteco.intervention_certification.value');
+                $total_intervention_certificated_price = $intervention_price + $supervision_price + $overhead_price + $business_profit_price + $intervention_certification;
+                $team_price = config('sisteco.team.value');
+                $platform_maintenance_price = $total_intervention_certificated_price * config('sisteco.platform_maintenance.value');
+                $total_intervention_gross_price = $total_intervention_certificated_price + $team_price + $platform_maintenance_price;
+                $total_intervention_net_price = $total_intervention_gross_price - ($total_intervention_gross_price * $vat / 100);
+                $total_intervention_vat = $total_intervention_gross_price - $total_intervention_net_price;
+                $intervention_gross_price_per_area = $total_intervention_gross_price / $intervention_area;
+
+                //create an array with all the variables above using number_format to format the numbers
+                $interventions['info'] = [
+                    'intervention_area' => number_format($intervention_area, 4, ',', '.'),
+                    'intervention_price' => number_format($intervention_price, 2, ',', '.'),
+                    'supervision_price' => number_format($supervision_price, 2, ',', '.'),
+                    'overhead_price' => number_format($overhead_price, 2, ',', '.'),
+                    'business_profit_price' => number_format($business_profit_price, 2, ',', '.'),
+                    'intervention_certification' => number_format($intervention_certification, 2, ',', '.'),
+                    'total_intervention_certificated_price' => number_format($total_intervention_certificated_price, 2, ',', '.'),
+                    'team_price' => number_format($team_price, 2, ',', '.'),
+                    'platform_maintenance_price' => number_format($platform_maintenance_price, 2, ',', '.'),
+                    'total_intervention_gross_price' => number_format($total_intervention_gross_price, 2, ',', '.'),
+                    'total_intervention_net_price' => number_format($total_intervention_net_price, 2, ',', '.'),
+                    'total_intervention_vat' => number_format($total_intervention_vat, 2, ',', '.'),
+                    'intervention_gross_price_per_area' => number_format($intervention_gross_price_per_area, 2, ',', '.'),
+                ];
+
+                //assign the fields to json
                 $json['interventions'] = $interventions;
                 $json['maintenance'] = $maintenance;
                 $json['general'] = $general;
