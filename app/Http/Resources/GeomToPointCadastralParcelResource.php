91d22fdfd63c0d79b6b3dd55cccfc8cbc384cf29
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-04-12 11:49:44 +0200

    Fixed geometry on api cadastral parcel

--- app/Http/Resources/GeomToPointCadastralParcelResource.php
+++ app/Http/Resources/GeomToPointCadastralParcelResource.php
@@ -15,19 +15,54 @@ class GeomToPointCadastralParcelResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $geometry = DB::select('select st_asGeojson(st_centroid(geometry)) as geom from cadastral_parcels where id=680;')[0]->geom;
+        $geometry = DB::select("select st_asGeojson(st_centroid(geometry)) as geom from cadastral_parcels where id=$this->id;")[0]->geom;
         return [
             'id' => $this->id,
             'name' => [
                 'it' => $this->code,
             ],
             'description' => [
-                'it' => 'TO BE IMPLEMENTED',
+                'it' => "<table>
+	<tbody>
+		<tr>
+			<td><strong>Area:</strong></td>
+			<td>$this->square_meter_surface</td>
+		</tr>
+		<tr>
+			<td><strong>Classe Pendenza:</strong></td>
+			<td>$this->slope</td>
+		</tr>
+		<tr>
+			<td><strong>Pendenza:</strong></td>
+			<td>$this->average_slope</td>
+		</tr>
+		<tr>
+			<td><strong>Classe Trasporto:</strong></td>
+			<td>$this->way</td>
+		</tr>
+		<tr>
+			<td><strong>Comune:</strong></td>
+			<td>$this->municipality</td>
+		</tr>
+		<tr>
+		</tr>
+	</tbody>
+    <h3>Interventi Forestali:</h3>
+<table>
+	<thead>
+		<tr>
+			<th>Codice</th>
+			<th>Costo Singolo</th>
+			<th>Costo Complessivo</th>
+		</tr>
+	</thead>
+</table>
+</table>"
             ],
             'related_url' => [
-                'https://sisteco.maphub.it/cadastral-parcels/'.$this->id,
+                'https://sisteco.maphub.it/cadastral-parcels/' . $this->id,
             ],
-            'geometry' => json_decode($geometry,true),
+            'geometry' => json_decode($geometry, true),
 
         ];
     }
