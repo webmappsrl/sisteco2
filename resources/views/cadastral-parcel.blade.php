68b51014e8e32d5f7e64565ab75e8edb03b898fc
Alessio Piccioli <piccioli@netseven.it>
2023-04-13 19:12:46 +0200

    Modified tag embed

--- resources/views/cadastral-parcel.blade.php
+++ resources/views/cadastral-parcel.blade.php
@@ -15,7 +15,7 @@
     <div class="parcel-details">
         <h1>Dettagli della Parcella</h1>
         <div style="position:relative;display:block;width:100%;height:200px;">
-            <app-map parcel="{{ $cadastralParcel->id }}"></app-map>
+            <app-root parcel="{{ $cadastralParcel->id }}"></app-root>
         </div>
 
         <table class="parcel-details-table">
