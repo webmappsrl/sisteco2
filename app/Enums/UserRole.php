e6cd22e94bad300b0caf4d7ac46a4010072679a9
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-04-17 15:53:27 +0200

    Implemented Roles system for users. Written tests for it.

---
+++ app/Enums/UserRole.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum UserRole: string
+{
+    case Admin = 'admin';
+    case Developer = 'developer';
+    case Manager = 'manager';
+    case Editor = 'editor';
+}
