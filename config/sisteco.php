63d387c3283c403bbd00fd532d5bea6de22bcb25
Alessio Piccioli <piccioli@netseven.it>
2023-11-09 16:41:07 +0100

    feat: Update sisteco.php with new color codes
    - Added color code for "Nessun intervento"
    - Added color code for "Diradamento"
    - Updated color code for "Avviamento alto fusto"
    - Added color code for "Taglio ceduo"
    - Added color code for "Recupero post-incendio"
    - Added color code for "Selvicultura ad albero"

--- config/sisteco.php
+++ config/sisteco.php
@@ -58,26 +58,32 @@ return [
             'strokeColor' => 'rgba(255, 0, 0)',
             'fillColor' => 'rgba(255, 0, 0, 0)',
         ],
+        // Nessun intervento
         '0' => [
             'strokeColor' => 'rgba(191, 191, 191, 1)',
             'fillColor' => 'rgba(208, 208, 208, 0.4)',
         ],
+        // Diradamento
         '1' => [
             'strokeColor' => 'rgba(255, 221, 1, 1)',
             'fillColor' => 'rgba(255, 221, 1, 0.4)',
         ],
+        // Avviamento alto fusto 
         '2' => [
-            'strokeColor' => 'rgba(255, 1, 14, 1)',
-            'fillColor' => 'rgba(255, 1, 14, 0.4)',
-        ],
-        '3' => [
             'strokeColor' => 'rgba(128, 86, 52, 1)',
             'fillColor' => 'rgba(128, 86, 52, 0.4)',
         ],
+        // Taglio ceduo
+        '3' => [
+            'strokeColor' => 'rgba(255, 1, 14, 1)',
+            'fillColor' => 'rgba(255, 1, 14, 0.4)',
+        ],
+        // Recupero post-incendio
         '4' => [
             'strokeColor' => 'rgba(219, 30, 210, 1)',
             'fillColor' => 'rgba(219, 30, 210, 0.4)',
         ],
+        // Selvicultura ad albero
         '5' => [
             'strokeColor' => 'rgba(128, 255, 0, 1)',
             'fillColor' => 'rgba(128, 255, 0, 0.4)',
