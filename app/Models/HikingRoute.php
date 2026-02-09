167387404293ca9f68607ffa845aced6e4e252c3
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2026-02-09 09:41:39 +0100

    chore: update asset links for stability oc: 7099
    Switches asset links to a stable path using jsDelivr for
    improved loading reliability in catalog-area views.
    
    Relates to oc_7099

--- app/Models/HikingRoute.php
+++ app/Models/HikingRoute.php
@@ -16,7 +16,8 @@ class HikingRoute extends Model
 
     public static function getListFromCai():array {
         // TODO: generalize for different bbox taken from catalog
-        $bbox='10.384123,43.679826,10.643226,43.847591';
+        // $bbox='10.384123,43.679826,10.643226,43.847591';
+        $bbox='10.3,43.5,10.7,43.9';
         $api = "https://osm2cai.cai.it/api/v2/hiking-routes/bb/$bbox/3,4";
         $list = json_decode(file_get_contents($api),TRUE);
         return $list;
@@ -24,6 +25,7 @@ class HikingRoute extends Model
 
     public static function importFromCai($cai_id) {
         $data = json_decode(file_get_contents("https://osm2cai.cai.it/api/v2/hiking-route/$cai_id"),TRUE);
+        $data = self::removeZFromGeoJSON($data);
         if(!is_null($data) && count($data)>0 && isset($data['properties']) && isset($data['geometry'])) {
             $geometry = json_encode($data['geometry']);
             return self::updateOrCreate(
@@ -38,4 +40,26 @@ class HikingRoute extends Model
                 );
         }
     }
+
+    public static function removeZFromGeoJSON($geoJSON) {
+
+    // Controlla se esiste 'geometry' e se il tipo è MultiLineString
+    if (isset($geoJSON['geometry']) && $geoJSON['geometry']['type'] === 'MultiLineString') {
+        // Itera su ogni LineString nel MultiLineString
+        foreach ($geoJSON['geometry']['coordinates'] as &$lineString) {
+            // Itera su ogni coordinata del LineString
+            foreach ($lineString as &$coordinate) {
+                // Rimuovi la coordinata Z se esiste (assumendo che ci siano 3 elementi per coordinata)
+                if (count($coordinate) === 3) {
+                    array_pop($coordinate); // Rimuove l'ultimo elemento (coordinata Z)
+                }
+            }
+        }
+    }
+
+    // Ritorna il GeoJSON modificato come stringa
+    return $geoJSON;
+  }
+
+
 }
